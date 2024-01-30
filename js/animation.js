c2ed7d5a9b3772b9ba2481e1882ef8f4d6c22676
amkhrjee <aniruddha.mukherji@outlook.com>
2024-01-31 00:36:29 +0530

    button ui done

--- js/animation.js
+++ js/animation.js
@@ -2,6 +2,8 @@ let buttons = document.querySelectorAll("button")
 let firstRowButtons = document.querySelectorAll(".first-row-btn")
 let menuOverlayButton = document.querySelector(".menu-overlay-button")
 let arrow = document.querySelector(".menu-overlay-button>img")
+let hiddenRow = document.querySelector('.hidden-row')
+let hiddenRowButton = document.querySelectorAll(".hidden-row-btn")
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -25,6 +27,7 @@ firstRowButtons.forEach(button => {
     })
 })
 
+let toggleArrow = false
 menuOverlayButton.addEventListener("click", () => {
     console.log("Arrow Clicked!");
     let currentAngle = (parseFloat(arrow.style.transform.replace(/[^0-9\-.,]/g, '')) || 0);
@@ -33,4 +36,36 @@ menuOverlayButton.addEventListener("click", () => {
         navigator.vibrate(200)
     arrow.style.transform = "rotate(" + (currentAngle + rotationAngle) + "deg)"
     initialAngle = rotationAngle
+
+    // toggle display hidden menu
+    if (!toggleArrow) {
+        hiddenRow.style.display = "grid"
+        setTimeout(() => {
+            hiddenRow.style.opacity = 1
+        }, 100)
+        hiddenRow.style.transform = "translateY(0)"
+        buttons.forEach(button => {
+            button.style.height = "4.6rem"
+        })
+        toggleArrow = true
+    } else {
+        hiddenRow.style.display = "none"
+        hiddenRow.style.opacity = 0
+        hiddenRow.style.transform = "translateY(-50px)"
+        buttons.forEach(button => {
+            button.style.height = "5.6rem"
+        })
+        toggleArrow = false
+    }
+})
+
+hiddenRowButton.forEach(button => {
+    button.addEventListener("click", () => {
+        if ("vibrate" in navigator)
+            navigator.vibrate(200)
+        button.style.borderRadius = "0px"
+        setTimeout(() => {
+            button.style.borderRadius = "16px"
+        }, 200)
+    })
 })
