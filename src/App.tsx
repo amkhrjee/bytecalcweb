d498454e23c083221a09c90296169defd1163dc0
amkhrjee <aniruddha.mukherji@outlook.com>
2024-04-12 04:21:22 +0530

    tailwind works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import { Button } from "@nextui-org/react";
+
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
