737b1c628d2d65921480ef648c82e825f5cc781e
amkhrjee <aniruddha.mukherji@outlook.com>
2024-04-12 19:31:35 +0530

    add: history modal

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,18 +1,48 @@
-import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
 import RestoreOutlinedIcon from "@mui/icons-material/RestoreOutlined";
 
 export default function Display() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Card>
       <CardHeader className="flex justify-between">
         <p>Last Ans: 123.34</p>
-        <Button isIconOnly aria-label="history">
+        <Button isIconOnly aria-label="history" onPress={() => onOpen()}>
           <RestoreOutlinedIcon />
         </Button>
       </CardHeader>
       <CardBody>
         <input className="outline-none border-none text-9xl p-0 h-fit w-full bg-inherit text-right font-inherit" />
       </CardBody>
+      <Modal backdrop="blur" isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">History</ModalHeader>
+              <ModalBody>
+                <h1>History goes here...</h1>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </Card>
   );
 }
