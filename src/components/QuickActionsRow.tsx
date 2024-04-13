1c301a90507f08aaf4f6d151653d976b932b2013
amkhrjee <aniruddha.mukherji@outlook.com>
2024-04-14 00:13:35 +0530

    fix: dropdown

--- src/components/QuickActionsRow.tsx
+++ src/components/QuickActionsRow.tsx
@@ -8,15 +8,27 @@ import {
   DropdownTrigger,
 } from "@nextui-org/react";
 import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+
 export default function QuickActionsRow() {
+  const bitwiseOperators = [
+    "<<",
+    ">>",
+    "AND",
+    "OR",
+    "XOR",
+    "XNOR",
+    "NOT",
+    "NAND",
+    "NOR",
+  ];
+
+  // const unaryOperators = []
+
   return (
     <ButtonGroup variant="shadow" className="w-full p-2">
       <Button className="w-full text-xl">log₂</Button>
       <Button className="w-full text-xl">mod</Button>
       <Button className="w-full text-xl">2^</Button>
-      {/* <Button className="w-full" isIconOnly>
-        <KeyboardArrowDownOutlinedIcon />
-      </Button> */}
       <Dropdown className="dark text-foreground bg-background" backdrop="blur">
         <DropdownTrigger>
           <Button className="w-full" isIconOnly>
@@ -24,26 +36,31 @@ export default function QuickActionsRow() {
           </Button>
         </DropdownTrigger>
         <DropdownMenu
-          // className="grid grid-cols-2"
+          title="Bitwise Operators"
           variant="faded"
           aria-label="Static Actions"
         >
-          {/* <h1>Hello</h1> */}
           <DropdownSection
             aria-label="Bitwise Operators"
             title="Bitwise Operators"
-            showDivider
+            // showDivider
+            classNames={{ group: "grid grid-cols-3 " }} // slots
+            itemClasses={{
+              base: "gap-4",
+            }}
+            as="div"
           >
-            <DropdownItem key="left shiftw">&lt;&lt;</DropdownItem>
-            <DropdownItem key="right shift">&gt;&gt;</DropdownItem>
-            <DropdownItem key="and">AND</DropdownItem>
-            <DropdownItem key="or">OR</DropdownItem>
-            <DropdownItem key="xor">XOR</DropdownItem>
-            <DropdownItem key="xnor">XNOR</DropdownItem>
-            <DropdownItem key="nor">NOR</DropdownItem>
-            <DropdownItem key="not">NOT</DropdownItem>
+            {bitwiseOperators.map((operator) => (
+              <DropdownItem as="div" key={operator}>
+                {operator}
+              </DropdownItem>
+            ))}
           </DropdownSection>
-          <DropdownSection aria-label="miscellaneous" title="Miscellaneous">
+          {/* <DropdownSection
+            as="div"
+            aria-label="miscellaneous"
+            title="Miscellaneous"
+          >
             <DropdownItem key="percentage">%</DropdownItem>
             <DropdownItem key="ln">
               log<sub>e</sub>
@@ -51,7 +68,7 @@ export default function QuickActionsRow() {
             <DropdownItem key="log10">
               log<sub>10</sub>
             </DropdownItem>
-          </DropdownSection>
+          </DropdownSection> */}
         </DropdownMenu>
       </Dropdown>
     </ButtonGroup>
